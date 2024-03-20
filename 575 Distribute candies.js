91c703ac4a0731046647e340259347d56117e2a8
Dhruv Patel <114987699+Dhruv942@users.noreply.github.com>
2024-03-20 11:09:29 +0530

    575 Distribute candies.js

---
+++ 575 Distribute candies.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} candyType
+ * @return {number}
+ */
+var distributeCandies = function(candyType) {
+     let max = candyType.length / 2;
+
+    let types_count = new Set(candyType).size;
+
+    if ( types_count <= max ) {
+        return types_count;
+    } else {
+        return max;
+    }
+
+};
