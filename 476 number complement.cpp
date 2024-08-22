851a5d144561841067906e7718e829c5f173504d
Dhruv Patel <114987699+Dhruv942@users.noreply.github.com>
2024-08-22 08:13:07 +0530

    476 number complement.cpp

---
+++ 476 number complement.cpp
@@ -0,0 +1,8 @@
+  long powerof2s = 2, temp = num;
+        
+        while(temp>>1) {
+            temp >>= 1;
+            powerof2s <<= 1;
+        }
+        
+        return powerof2s - num - 1;
